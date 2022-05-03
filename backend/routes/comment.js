534667846baccb2ee66cbb65ab77306aaa685cf9
Moelgh <89198534+Moelgh@users.noreply.github.com>
2022-05-03 22:14:26 +0200

    Delete backend directory

--- backend/routes/comment.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middleware/auth');
-const commentCtrl = require('../controllers/comments');
-router.post('/comments', commentCtrl.createComment);
-router.get('/comments', auth, commentCtrl.getComments);
-router.delete('/comments/:id', auth, commentCtrl.deleteComment);
-
-module.exports = router;
